ba36e8ec9ca4d7e35c9ae21049633643e6eea5c2
hasanbadr <hasan.badr@edu.keuda.fi>
2023-12-12 08:00:06 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Syötä suurempi kokonaisluku kuin 10: ");
+        int luku = int.Parse(Console.ReadLine());
+
+        if (luku > 10)
+        {
+            for (int i = 1; i <= 13; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        Console.Write(luku);
+                        if (j < i - 1)
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("Syötä suurempi luku kuin 10.");
+        }
+    }
+}
